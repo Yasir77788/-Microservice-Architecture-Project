bffa3a19f290223879e67e374ee497047c0f1308
Yasir_H <45829991+Yasir77788@users.noreply.github.com>
2022-06-30 02:31:14 -0500

    Update DepartmentServiceApplicationTests.java

--- Department_Service/DEPARTMENT-SERVICE/src/test/java/com/fdmgroup/beach_project/department/DepartmentServiceApplicationTests.java
+++ Department_Service/DEPARTMENT-SERVICE/src/test/java/com/fdmgroup/beach_project/department/DepartmentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fdmgroup.beach_project.department;
+package com.microservice_project.department;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
